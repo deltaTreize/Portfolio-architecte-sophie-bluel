1356aaa1640418e52fa6a4d273ee0baeaff020e6
deltaTreize <ludo.leblond13@gmail.com>
2023-12-12 20:12:41 +0100

    modal2 post fini tout fonctionne bien (post - delete - get - login) reste des détails à améliorer

--- FrontEnd/frontEndApp.js
+++ FrontEnd/frontEndApp.js
@@ -5,9 +5,19 @@ const login = document.querySelector(".login");
 const filterBar = document.querySelector(".filterBar");
 const modifierProjet = document.querySelector(".modifier-projet");
 const modal1 = document.querySelector(".modal1");
-const modalWindow = document.querySelector(".modal-window");
-const modalCross = document.querySelector(".modal1-cross");
-const modalBtn = document.getElementById("modal1-btn");
+const modalWindow = document.querySelector(".modal-window1");
+const modalWindow2 = document.querySelector(".modal-window2");
+const modalArrow = document.querySelector(".modal-arrow");
+const modalCross1 = document.getElementById("modal1-cross");
+const modalCross2 = document.getElementById("modal2-cross");
+const modal1Btn = document.getElementById("modal1-btn");
+const modal2Btn = document.getElementById("modal2-btn");
+const pictureAddProjet = document.getElementById("picture");
+const formAddProjet = document.getElementById("formAddProjet");
+const titleAddProjet = document.querySelector(".inputAddProjet");
+const preview = document.querySelector(".preview");
+const importImg = document.querySelector(".importImg");
+const category = document.getElementById("category");
 const token = localStorage.getItem("token");
 const admin = document.querySelectorAll(".admin");
 const adminArray = Array.from(admin);
@@ -16,6 +26,10 @@ const noAdminArray = Array.from(noAdmin);
 const modalProjets = document.getElementById("modal1-projets");
 const modalFigure = modalProjets.childNodes;
 const modalFigureArray = [...modalFigure];
+let Modal0 = null;
+const focusPossible = "button, input";
+let focusable = [];
+let lastFocus = null;
 
 if (token) {
     adminArray.forEach((element) => {
@@ -73,37 +87,79 @@ async function createProjet(arg) {
 createProjet();
 
 /********* fenetre modale *********/
-let Modal0 = null;
+
+function changeFocusable() {
+    if (modalWindow2.style.display === "block") {
+        focusable = [...modalWindow2.querySelectorAll(focusPossible)];
+    } else {
+        focusable = [...modalWindow.querySelectorAll(focusPossible)];
+    }
+}
+
+const focusAction = (e) => {
+    e.preventDefault();
+    let index = focusable.findIndex(
+        (f) => f === modal1.querySelector(":focus")
+    );
+    if (e.shiftKey === true) {
+        index--;
+    } else {
+        index++;
+    }
+    if (index >= focusable.length) {
+        index = 0;
+    }
+    if (index < 0) {
+        index = focusable.length - 1;
+    }
+    focusable[index].focus();
+};
 
 const openModal = (e) => {
     e.preventDefault();
-    const target = document.querySelector(e.target.getAttribute("href"));
-    console.log(target);
-    target.style.display = "flex";
-    target.removeAttribute('aria-hidden');
-    target.setAttribute('aria-modal', 'true');
-    Modal0 = target;
+    lastFocus = document.querySelector(":focus");
+    changeFocusable();
+    focusable[0].focus();
+    Modal0 = document.querySelector(e.target.getAttribute("href"));
+    Modal0.style.display = "flex";
+    Modal0.removeAttribute("aria-hidden");
+    Modal0.setAttribute("aria-modal", "true");
     Modal0.addEventListener("click", closeModal);
     modalWindow.addEventListener("click", stopPropagation);
+    modalWindow2.addEventListener("click", stopPropagation);
 };
 
 const closeModal = (e) => {
+    if (Modal0 === null) {
+        return;
+    }
     e.preventDefault();
     Modal0.style.display = "none";
-    Modal0.setAttribute('aria-hidden', 'true');
-    Modal0.removeAttribute('aria-modal');
+    modalWindow2.style.display = "none";
+    modalWindow.style.display = "block";
+    Modal0.setAttribute("aria-hidden", "true");
+    Modal0.removeAttribute("aria-modal");
     Modal0.removeEventListener("click", closeModal);
     modalWindow.removeEventListener("click", stopPropagation);
-}
+    modalWindow2.removeEventListener("click", stopPropagation);
+    Modal0 = null;
+    if (lastFocus !== null) {
+        lastFocus.focus();
+    }
+};
 
-const stopPropagation = (e) =>{
+window.addEventListener("keydown", (e) => {
+    if (e.key === "Escape" || e.key === "Esc") {
+        closeModal(e);
+    }
+    if (e.key === "Tab" && modal1.style.display === "flex") {
+        focusAction(e);
+    }
+});
+
+const stopPropagation = (e) => {
     e.stopPropagation();
-}
-modifierProjet.addEventListener("click", openModal);
-modalCross.addEventListener("click", closeModal);
-modal1.addEventListener("click", closeModal);
-
-
+};
 
 async function deleteModalPicture(arg) {
     const fetchResult = await fetch(`http://localhost:5678/api/works/${arg}`, {
@@ -144,3 +200,77 @@ async function createModalPicture() {
 }
 createModalPicture();
 
+
+function previewFile() {
+    const file = pictureAddProjet.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+
+        reader.onload = function (e) {
+            const imageDataUrl = e.target.result;
+            preview.src = imageDataUrl;
+            if (preview.src) {
+                importImg.style.display = "none";
+                preview.style = "z-index: 0;";
+            }
+        };
+
+        reader.readAsDataURL(file);
+    }
+}
+
+function cleanForm() {
+    preview.src = "";
+    preview.style = "z-index: -1;";
+    importImg.style.display = "flex";
+    titleAddProjet.value = "";
+    category.value = "0";
+}
+
+async function createNewProjet() {
+    const formData = new FormData(formAddProjet);
+
+    fetch("http://localhost:5678/api/works", {
+        method: "POST",
+        headers: {
+            Authorization: "Bearer " + `${token}`,
+        },
+        body: formData,
+    })
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+            return response.json();
+        })
+        .then((data) => {
+            console.log("Success:", data);
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+        });
+
+        cleanForm();
+}
+
+modifierProjet.addEventListener("click", openModal);
+modalCross1.addEventListener("click", closeModal);
+modalCross2.addEventListener("click", closeModal);
+modal1.addEventListener("click", closeModal);
+modal1Btn.addEventListener("click", () => {
+    modalWindow.style.display = "none";
+    modalWindow2.style.display = "block";
+    changeFocusable();
+});
+modalArrow.addEventListener("click", () => {
+    modalWindow.style.display = "block";
+    modalWindow2.style.display = "none";
+    preview.src = "";
+    preview.style = "z-index: -1;";
+    importImg.style.display = "flex";
+    cleanForm();
+    changeFocusable();
+});
+modal2Btn.addEventListener("click", createNewProjet);
+pictureAddProjet.addEventListener("change", previewFile);
