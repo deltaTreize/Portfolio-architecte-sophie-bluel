8d2116d057cfb34c3296ae6c344ba509532d4bdf
deltaTreize <ludo.leblond13@gmail.com>
2024-03-16 16:05:00 +0100

    modif pour mise en ligne avec vercel

--- FrontEnd/login/login.js
+++ FrontEnd/login/login.js
@@ -5,32 +5,31 @@ const formconnection = document.getElementById("formconnection");
 const formconnectionInput = document.querySelectorAll("#formconnection input");
 const inputs = Array.from(formconnectionInput);
 
-formconnection.addEventListener("submit", users);
 
 async function users(e) {
-    e.preventDefault();
-    const response = await fetch("http://localhost:5678/api/users/login", {
-        method: "POST",
-        headers: { "content-type": "application/json" },
-        body: JSON.stringify({
-            email: `${email.value}`,
-            password: `${mdp.value}`,
-        }),
-    });
-    const usersjson = await response.json();
-    const token = usersjson.token;
-    localStorage.setItem("token", token);
-    if (token) {
-        window.location.replace("../index.html");
-    } else {
-         if (usersjson.error) {
-            erreur.innerText = "mot de passe n'est pas valide!";
-            erreur.style.backgroundColor = "red";
-            erreur.style.color = "#fff";
-        } else if (usersjson.message) {
-            erreur.innerText = "l'email n'est pas valide!";
-            erreur.style.backgroundColor = "red";
-            erreur.style.color = "#fff";
-        }
-    }
+	e.preventDefault();
+	const response = await fetch("http://localhost:5678/api/users/login", {
+		method: "POST",
+		headers: { "content-type": "application/json" },
+		body: JSON.stringify({
+			email: `${email.value}`,
+			password: `${mdp.value}`,
+		}),
+	});
+	const usersjson = await response.json();
+	const token = usersjson.token;
+	localStorage.setItem("token", token);
+	if (token) {
+		window.location.replace("../index.html");
+	} else if (usersjson.error) {
+		erreur.innerText = "mot de passe n'est pas valide!";
+		erreur.style.backgroundColor = "red";
+		erreur.style.color = "#fff";
+	} else if (usersjson.message) {
+		erreur.innerText = "l'email n'est pas valide!";
+		erreur.style.backgroundColor = "red";
+		erreur.style.color = "#fff";
+	}
 }
+
+formconnection.addEventListener("submit", users);
